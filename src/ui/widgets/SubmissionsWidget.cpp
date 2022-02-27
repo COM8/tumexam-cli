7b07c32f62e1824eab1cde3f2556776919d06347
Fabian Sauter <sauter.fabian@mailbox.org>
2022-02-27 09:50:28 +0100

    Updating submissions in seperate thread

--- src/ui/widgets/SubmissionsWidget.cpp
+++ src/ui/widgets/SubmissionsWidget.cpp
@@ -11,32 +11,37 @@
 #include <cstddef>
 #include <memory>
 #include <optional>
+#include <gtkmm/adjustment.h>
 #include <gtkmm/box.h>
 #include <gtkmm/enums.h>
 #include <gtkmm/orientable.h>
 
 namespace ui::widgets {
-SubmissionsWidget::SubmissionsWidget(Gtk::Window* window) : Gtk::Box(Gtk::Orientation::VERTICAL), window(window) { prep_widget(); }
+SubmissionsWidget::SubmissionsWidget(Gtk::Window* window) : Gtk::Box(Gtk::Orientation::VERTICAL), window(window) {
+    prep_widget();
+    submissionsChangedDisp.connect(sigc::mem_fun(*this, &SubmissionsWidget::on_notification_from_update_thread));
+    isUpdatingChangedDisp.connect(sigc::mem_fun(*this, &SubmissionsWidget::on_is_updating_changed_from_thread));
+    start_thread();
+}
+
+SubmissionsWidget::~SubmissionsWidget() { stop_thread(); }
 
 void SubmissionsWidget::set_credentials(std::shared_ptr<backend::tumexam::Credentials> credentials) { this->credentials = std::move(credentials); }
 
 void SubmissionsWidget::prep_widget() {
-    updateProgBar.hide();
-    append(updateProgBar);
-
     Gtk::Box* actionsBox = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::HORIZONTAL);
     actionsBox->set_margin(10);
     append(*actionsBox);
     updateBtn.set_icon_name("view-refresh");
     updateBtn.set_tooltip_text("Update");
-    updateBtn.signal_clicked().connect(sigc::mem_fun(*this, &SubmissionsWidget::update));
+    updateBtn.signal_clicked().connect(sigc::mem_fun(*this, &SubmissionsWidget::on_update_clicked));
     actionsBox->append(updateBtn);
     autoUpdateSwitch.set_tooltip_text("Enable automatic updates");
     autoUpdateSwitch.set_margin_start(10);
     autoUpdateSwitch.set_active(true);
     actionsBox->append(autoUpdateSwitch);
     updateIntervallEntry.set_margin_start(10);
-    updateIntervallEntry.set_increments(1, 60);
+    updateIntervallEntry.set_adjustment(Gtk::Adjustment::create(5, 1, 600, 1, 10));
     updateIntervallEntry.set_tooltip_text("Automatic update intervall in seconds");
     actionsBox->append(updateIntervallEntry);
     updateLbl.set_margin_start(10);
@@ -46,39 +51,6 @@ void SubmissionsWidget::prep_widget() {
     submissionsScroll.set_child(submissionsFlowBox);
     submissionsScroll.set_vexpand(true);
     append(submissionsScroll);
-}
-
-void SubmissionsWidget::update() {
-    if (!credentials) {
-        // TODO: Show warning
-        return;
-    }
-
-    updateProgBar.show();
-    updateProgBar.pulse();
-    updateBtn.set_sensitive(false);
-
-    std::optional<backend::tumexam::Submissions> subs = backend::tumexam::get_submission_status(*credentials);
-    if (subs) {
-        SPDLOG_INFO("Found {} student submissions.", subs->students.size());
-        updateLbl.set_text(get_cur_time() + " - Success");
-        submissions = std::make_unique<backend::tumexam::Submissions>(std::move(*subs));
-        // Sort submissions:
-        std::sort(submissions->students.begin(), submissions->students.end(),
-                  [](const std::shared_ptr<backend::tumexam::SubmissionStudent>& a, const std::shared_ptr<backend::tumexam::SubmissionStudent>& b) {
-                      return a->get_state() < b->get_state();
-                  });
-        clear_submissions();
-        for (const std::shared_ptr<backend::tumexam::SubmissionStudent>& submission : submissions->students) {
-            add_session_button(submission);
-        }
-    } else {
-        updateLbl.set_text(get_cur_time() + " - No submissions found");
-        SPDLOG_INFO("No student submissions found.");
-    }
-
-    updateProgBar.hide();
-    updateBtn.set_sensitive(true);
 }
 
 void SubmissionsWidget::add_session_button(std::shared_ptr<backend::tumexam::SubmissionStudent> submission) {
@@ -91,7 +63,7 @@ void SubmissionsWidget::add_session_button(std::shared_ptr<backend::tumexam::Sub
 
 void SubmissionsWidget::clear_submissions() {
     for (Gtk::FlowBoxChild& child : submissionFlowBoxWidgets) {
-        submissionsFlowBox.remove(*(child.get_parent()));
+        submissionsFlowBox.remove(child);
     }
     submissionFlowBoxWidgets.clear();
     submissionWidgets.clear();
@@ -101,4 +73,106 @@ std::string SubmissionsWidget::get_cur_time() {
     std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
     return date::format("%T", date::floor<std::chrono::seconds>(tp));
 }
+
+void SubmissionsWidget::start_thread() {
+    assert(!updateThread);
+    assert(!shouldRun);
+    shouldRun = true;
+    updateThread = std::make_unique<std::thread>(&SubmissionsWidget::thread_run, this);
+}
+
+void SubmissionsWidget::stop_thread() {
+    assert(updateThread);
+    assert(shouldRun);
+    shouldRun = false;
+    updateThread->join();
+    updateThread = nullptr;
+}
+
+void SubmissionsWidget::thread_run() {
+    SPDLOG_INFO("Submissions thread started.");
+    std::chrono::steady_clock::time_point lastUpdate = std::chrono::steady_clock::now();
+    while (shouldRun) {
+        std::chrono::steady_clock::duration d = std::chrono::steady_clock::now() - lastUpdate;
+        if (shouldUpdateOnce || (autoUpdateSwitch.get_active() && (d > std::chrono::seconds(static_cast<int>(updateIntervallEntry.get_value()))))) {
+            shouldUpdateOnce = false;
+            update_submissions();
+            lastUpdate = std::chrono::steady_clock::now();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    SPDLOG_INFO("Submissions thread stoped.");
+}
+
+void SubmissionsWidget::update_submissions() {
+    if (!credentials) {
+        isUpdating = false;
+        isUpdatingChangedDisp.emit();
+        SPDLOG_WARN("Updating submissions canceled - invalid credentials.");
+        return;
+    }
+    SPDLOG_INFO("Updating submissions...");
+
+    isUpdating = true;
+    isUpdatingChangedDisp.emit();
+    std::optional<backend::tumexam::Submissions> subs = backend::tumexam::get_submission_status(*credentials);
+    submissionsMutex.lock();
+    if (subs) {
+        submissions = std::make_unique<backend::tumexam::Submissions>(std::move(*subs));
+        SPDLOG_INFO("Found {} student submissions.", submissions->students.size());
+    } else {
+        submissions = nullptr;
+        SPDLOG_INFO("No student submissions found.");
+    }
+    submissionsMutex.unlock();
+    submissionsChangedDisp.emit();
+    isUpdating = false;
+    isUpdatingChangedDisp.emit();
+}
+
+void SubmissionsWidget::update_submissions_ui() {
+    submissionsMutex.lock();
+    if (submissions) {
+        updateLbl.set_text(get_cur_time() + " - Success");
+        // Sort submissions:
+        std::sort(submissions->students.begin(), submissions->students.end(),
+                  [](const std::shared_ptr<backend::tumexam::SubmissionStudent>& a, const std::shared_ptr<backend::tumexam::SubmissionStudent>& b) {
+                      return a->get_state() < b->get_state();
+                  });
+        clear_submissions();
+        for (const std::shared_ptr<backend::tumexam::SubmissionStudent>& submission : submissions->students) {
+            add_session_button(submission);
+        }
+    } else {
+        updateLbl.set_text(get_cur_time() + " - No submissions found");
+    }
+    submissionsMutex.unlock();
+}
+
+void SubmissionsWidget::update_is_updating_ui() {
+    if (isUpdating) {
+        updatingSpinner.start();
+        updateBtn.set_child(updatingSpinner);
+        updateBtn.set_sensitive(false);
+    } else {
+        updatingSpinner.stop();
+        updateBtn.set_sensitive(true);
+        updateBtn.set_icon_name("view-refresh");
+    }
+
+    if (!credentials) {
+        updateLbl.set_text(get_cur_time() + " - No valid credentials");
+        return;
+    }
+}
+
+void SubmissionsWidget::on_update_clicked() {
+    shouldUpdateOnce = true;
+    isUpdating = true;
+    update_is_updating_ui();
+}
+
+//-----------------------------Events:-----------------------------
+void SubmissionsWidget::on_notification_from_update_thread() { update_submissions_ui(); }
+void SubmissionsWidget::on_is_updating_changed_from_thread() { update_is_updating_ui(); }
 }  // namespace ui::widgets
--- src/ui/widgets/SubmissionsWidget.hpp
+++ src/ui/widgets/SubmissionsWidget.hpp
@@ -4,25 +4,36 @@
 #include "backend/tumexam/Submissions.hpp"
 #include "ui/widgets/SubmissionWidget.hpp"
 #include <memory>
+#include <mutex>
+#include <thread>
 #include <vector>
 #include <gtkmm.h>
 #include <gtkmm/label.h>
 #include <gtkmm/progressbar.h>
 #include <gtkmm/scrolledwindow.h>
 #include <gtkmm/spinbutton.h>
+#include <gtkmm/spinner.h>
 #include <gtkmm/switch.h>
 #include <gtkmm/togglebutton.h>
 
 namespace ui::widgets {
 class SubmissionsWidget : public Gtk::Box {
  private:
+    std::unique_ptr<std::thread> updateThread{nullptr};
+    std::mutex submissionsMutex{};
+    bool shouldRun{false};
+    Glib::Dispatcher submissionsChangedDisp;
+    bool isUpdating{false};
+    Glib::Dispatcher isUpdatingChangedDisp;
+    bool shouldUpdateOnce{false};
+
     std::shared_ptr<backend::tumexam::Credentials> credentials;
     std::unique_ptr<backend::tumexam::Submissions> submissions;
     std::vector<SubmissionWidget> submissionWidgets;
     std::vector<Gtk::FlowBoxChild> submissionFlowBoxWidgets;
 
     Gtk::Button updateBtn;
-    Gtk::ProgressBar updateProgBar;
+    Gtk::Spinner updatingSpinner;
     Gtk::Label updateLbl;
     Gtk::SpinButton updateIntervallEntry;
     Gtk::Switch autoUpdateSwitch;
@@ -33,16 +44,27 @@ class SubmissionsWidget : public Gtk::Box {
 
  public:
     explicit SubmissionsWidget(Gtk::Window* window);
+    ~SubmissionsWidget() override;
 
     void set_credentials(std::shared_ptr<backend::tumexam::Credentials> credentials);
 
  private:
     void prep_widget();
-    void update();
     void add_session_button(std::shared_ptr<backend::tumexam::SubmissionStudent> submission);
     void clear_submissions();
     static std::string get_cur_time();
 
+    void start_thread();
+    void stop_thread();
+    void thread_run();
+
+    void update_submissions();
+    void update_submissions_ui();
+    void update_is_updating_ui();
+
     //-----------------------------Events:-----------------------------
+    void on_notification_from_update_thread();
+    void on_is_updating_changed_from_thread();
+    void on_update_clicked();
 };
 }  // namespace ui::widgets
