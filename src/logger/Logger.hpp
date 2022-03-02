cb7830688a156e9ca7e7221a66b09db8b6b4bbe9
Fabian Sauter <sauter.fabian@mailbox.org>
2022-03-02 10:54:11 +0100

    Changed the log level option

--- src/logger/Logger.hpp
+++ src/logger/Logger.hpp
@@ -5,7 +5,7 @@
 
 namespace logger {
 const std::filesystem::path log_folder("logs");
-// Setup the logger, note the loglevel can not be set below the CMAKE log level (To change this use -DLOG_LEVEL=...)
+// Setup the logger, note the loglevel can not be set below the CMAKE log level (To change this use -DTUMEXAM_CLI_LOG_LEVEL=...)
 void setup_logger(const spdlog::level::level_enum level);
 void set_log_level(const spdlog::level::level_enum level);
 void deactivate_logger();
